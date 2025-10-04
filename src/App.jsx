a2b8cf613259a8d0ff2a5cfb1d495fdaad7fba4b
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-10-04 11:48:52 +0800

    Completed all required tasks for the practical exam

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import './App.css'
-
 function App() {
   return (
     <>
       <>
-  {/* ***** Preloader Start ***** */}
-  <div id="js-preloader" className="js-preloader">
-    <div className="preloader-inner">
-      <span className="dot" />
-      <div className="dots">
-        <span />
-        <span />
-        <span />
-      </div>
-    </div>
-  </div>
-  {/* ***** Preloader End ***** */}
   {/* ***** Header Area Start ***** */}
   <header
     className="header-area header-sticky wow slideInDown"
